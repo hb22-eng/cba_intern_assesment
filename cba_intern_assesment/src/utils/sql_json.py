51545bbee4269d6d7f505c3583b9fc39abe5ed62
hb22-eng <133706491+hb22-eng@users.noreply.github.com>
2023-05-18 23:51:51 -0500

    Add files via upload

---
+++ cba_intern_assesment/src/utils/sql_json.py
@@ -0,0 +1,31 @@
+import json
+import pandas as pd
+from sqlalchemy import create_engine
+
+#Database credentials
+user='root'
+password='password'
+host= 'localhost'
+database = 'lou'
+
+
+class DataMethods:
+
+    def get_transactional_data_date(transaction_date):
+        engine = create_engine("mysql+pymysql://{user}:{password}@{host}/{database}")
+
+        #Now we are quering the database with pandas
+        #Query is just the string to filter
+        query = f"SELECT * FROM transactional_data WHERE transaction_date <= '{transaction_date}'"
+        # this takes filter and the engine to connect it
+        re = pd.read_sql_query(query,engine)
+
+        #Converts to dictionary.
+        data= re.to_dict(orient='records')
+
+        #Convert to json
+        json_convert =json.dumps(data)
+
+        return json_convert
+    
+
